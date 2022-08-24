6e7dae5c9498e04baf97c398974c4faff53c10a8
aniket2910 <solankianiket0411@gmail.com>
2022-08-24 16:38:33 +0530

    update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await connect(process.env.monogoURL);
+    await connect(
+      "mongodb+srv://aniket2910:DgBNCSrqC3g1tLYQ@cluster0.hqr76ud.mongodb.net/todo-app?retryWrites=true&w=majority"
+    );
     console.log("Connnected to MongoDB");
   } catch (e) {
     console.log(e);
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   try {
     connectDB();
